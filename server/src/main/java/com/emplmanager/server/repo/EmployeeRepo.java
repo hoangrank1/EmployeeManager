8e2b3da50519782e6ebb3537dae13d6101553f6a
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-08-04 19:04:54 +0700

    server implementation

---
+++ server/src/main/java/com/emplmanager/server/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.emplmanager.server.repo;
+
+import com.emplmanager.server.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
